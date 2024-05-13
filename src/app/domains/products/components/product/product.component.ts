cb0f5a477ae69611564ed266b738caee1493157c
mloherr <herraizmaria08@gmail.com>
2024-05-13 14:09:29 +0200

    Added main structure to products and list using Tailwind!

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,5 +9,15 @@ import { CommonModule } from '@angular/common';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-  randomImage = 'https://picsum.photos/640/640?r=' + Math.random();
+  @Input({required: true}) img: string = '';
+  @Input({required: true}) price: number = 0;
+  @Input({required: true}) title: string = '';
+
+  @Output() addToCart = new EventEmitter();
+
+  addToCartHandler(){
+    console.log('Click from child');
+    this.addToCart.emit('Hola, este es un mensaje desde el hijo ' + this.title)
+  };
+
 }
