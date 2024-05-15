a9a968770cb13f4580fba19434104d170770ea4b
mloherr <herraizmaria08@gmail.com>
2024-05-15 11:16:36 +0200

    Defined query params for filter by category on home page

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,15 +1,18 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, Input, signal, SimpleChange, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from '@products/components/product/product.component';
 import { Product } from '@shared/models/product.model';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
+import { CategoryService } from '@shared/services/category.service';
+import { Category } from '@shared/models/category.model';
+import { RouterLinkWithHref } from '@angular/router';
 
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, RouterLinkWithHref],
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
@@ -17,23 +20,46 @@ import { ProductService } from '@shared/services/product.service';
 
 export class ListComponent {
   products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
   private cartService = inject(CartService);
   private productService = inject(ProductService);
+  private categoryService = inject(CategoryService);
+  @Input() category_id?: string;
 
   ngOnInit() {
-    this.productService.getProducts()
+    this.getCategories();
+  };
+
+  ngOnChanges(changes: SimpleChanges) {
+      this.getProducts();
+  };
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product)
+  };
+
+  private getProducts(){
+    this.productService.getProducts(this.category_id)
     .subscribe({
       next: (products) => {
         this.products.set(products);
       },
       error: () => {
-
       }
     })
-  }
+  };
 
-  addToCart(product: Product) {
-    this.cartService.addToCart(product)
-  }
+  private getCategories(){
+    this.categoryService.getAllCategories()
+    .subscribe({
+      next: (data) => {
+        this.categories.set(data);
+      },
+      error: () => {
+      }
+    })
+  };
+
+
 }
 
