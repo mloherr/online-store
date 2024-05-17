14a8248d8d61ecf3d96b12944453832c73e6db5f
mloherr <herraizmaria08@gmail.com>
2024-05-17 09:21:18 +0200

    Removed unused components

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,28 +1,13 @@
-import { Component, signal } from '@angular/core';
-
-import { WaveAudioComponent } from './../../../info/components/wave-audio/wave-audio.component';
+import { Component } from '@angular/core';
 
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [WaveAudioComponent],
+  imports: [],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-  duration = signal(1000);
-  message = signal('Holi!');
-
-  changeDuration = (event: Event) => {
-    const input = event.target as HTMLInputElement;
-    this.duration.set(input.valueAsNumber);
-  };
-  changeMessage= (event: Event) => {
-    const input = event.target as HTMLInputElement;
-    this.message.set(input.value);
-  };
-
-
 
 }
